1b8dc7299ff952cc9b00bc1af24cb6b5ad890a04
quote-meta <46474530+quote-meta@users.noreply.github.com>
2020-12-16 00:56:18 +0900

    add model handler

---
+++ src/main/java/quote/fsRod/client/core/handler/ModelHandler.java
@@ -0,0 +1,30 @@
+package quote.fsRod.client.core.handler;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import quote.fsRod.client.model.IHasCustomModel;
+import quote.fsRod.common.lib.LibMisc;
+
+@Mod.EventBusSubscriber(value = Side.CLIENT, modid = LibMisc.MOD_ID)
+public class ModelHandler {
+
+    private ModelHandler(){}
+
+    public static final ModelHandler INSTANCE = new ModelHandler();
+
+    @SideOnly(Side.CLIENT)
+    private final net.minecraft.client.renderer.block.statemap.DefaultStateMapper stateMapper = new net.minecraft.client.renderer.block.statemap.DefaultStateMapper();
+
+    @SubscribeEvent
+    public static void registerCustomModels(ModelRegistryEvent event){
+        for(Item item : Item.REGISTRY) {
+            if(item instanceof IHasCustomModel){
+                ((IHasCustomModel)item).registerCustomModel();
+            }
+        }
+    }
+}
